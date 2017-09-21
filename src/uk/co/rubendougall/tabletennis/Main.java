1060f894b7569a2f56b605206ab6c0ff72ac7c23
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-09-21 13:22:23 +0100

    Add game loop & calculate delta time

--- src/uk/co/rubendougall/tabletennis/Main.java
+++ src/uk/co/rubendougall/tabletennis/Main.java
@@ -1,5 +1,6 @@
 package uk.co.rubendougall.tabletennis;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -19,14 +20,21 @@ public class Main extends Application {
         Group root = new Group();
         Canvas canvas = new Canvas(800, 500);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        draw(gc);
+
+        final long startTime = System.nanoTime();
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                double delta = (now - startTime) / 1000000000.0;
+            }
+        }.start();
 
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
-    private void draw(GraphicsContext gc) {
+    private void draw(GraphicsContext gc, double delta) {
 
     }
 }
