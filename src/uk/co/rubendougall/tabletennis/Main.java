8e9db69fa245efd8d509d34fe02cd2f0d6cd2100
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-09-21 14:20:30 +0100

    Draw centre line

--- src/uk/co/rubendougall/tabletennis/Main.java
+++ src/uk/co/rubendougall/tabletennis/Main.java
@@ -50,6 +50,12 @@ public class Main extends Application {
         Point2D edgeSpacing = new Point2D(20, 20);
         gc.strokeRect(edgeSpacing.getX(), edgeSpacing.getY(), canvas.getWidth() - (2 * edgeSpacing.getX()),
                 canvas.getHeight() - (2 * edgeSpacing.getY()));
+
+        // Draw centre line
+        gc.setStroke(Color.grayRgb(140));
+        double centreLineSpacing = 30;
+        gc.strokeLine(canvas.getWidth() / 2, centreLineSpacing, canvas.getWidth() / 2,
+                canvas.getHeight() - centreLineSpacing);
     }
 
     private void draw(GraphicsContext gc, double delta) {
